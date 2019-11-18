a418c0a3186bebf92f80e8fb91f32a7ff44b9067
Mike <mikey@jigsaw.xyz>
2019-11-17 21:15:31 -0500

    able to add deadlines to game

--- services/firebase/current-games/start-new-game.js
+++ services/firebase/current-games/start-new-game.js
@@ -4,8 +4,12 @@ const { Timestamp } = require('firebase-admin').firestore;
 const db = admin.firestore();
 
 const startNewGameInDatabase = async (gameId) => {
+  const startTimestamp = new Date();
+  const deadlineDate = new Date();
+  const deadlineTimestamp = new Date(deadlineDate.setDate(deadlineDate.getDate() + 7));
   const data = {
-    game_started: Timestamp.fromDate(new Date()),
+    game_started: Timestamp.fromDate(startTimestamp),
+    game_deadline: Timestamp.fromDate(deadlineTimestamp),
     status: 'PLAYING',
   };
   try {
